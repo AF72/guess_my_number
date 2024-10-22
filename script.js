5db5b02aae67649e7c67226e3fa87f225e6bc24f
Adriano Fernandes <ajmf72@yahoo.com>
2024-10-22 14:34:58 +0100

    Eliminate duplicated coding. Adjust same styles.

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ let score = 20;
 console.log(secretNumber);
 
 //Function for guess > secretNumber or guess < secretNumber
-function testGuess(params) {}
+function testGuess(params) {
+  const stateOfGuess = params ? 'Too High!' : 'Too Low';
+  document.querySelector('.message').textContent = `Number ${stateOfGuess}`;
+  score--;
+  document.querySelector('.score').textContent = score;
+}
 
 //The next peace of code check the logic of the game
 document.querySelector('.check').addEventListener('click', function () {
@@ -17,23 +22,12 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correcto Number...👍';
     document.querySelector('.number').textContent = secretNumber;
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Number Too High!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You lost the game!😘';
-      document.querySelector('.score').textContent = 0;
-    }
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = ' Number Too Low!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You lost the game!😘';
-      document.querySelector('.score').textContent = 0;
-    }
+  } else if (guess !== secretNumber) {
+    const number = guess > secretNumber ? true : false;
+    testGuess(number);
+  }
+  if (score === 0) {
+    document.querySelector('.message').textContent = 'You lost the game!😘';
+    document.querySelector('.score').textContent = 0;
   }
 });
